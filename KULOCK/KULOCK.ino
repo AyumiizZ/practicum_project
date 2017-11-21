2d17a49d81298fc9216f44c703d6ff2545ba9573
AyumiizZ <korrawitc@gmail.com>
2017-11-21 17:38:09 +0700

    ID/PWD finished

--- KULOCK/KULOCK.ino
+++ KULOCK/KULOCK.ino
@@ -1,11 +1,31 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
+#include <avr/pgmspace.h>
 #define buttonPinLOCK PIN_PD6
 #define buttonPinID PIN_PB3
-#define IDLENGTH 7
-const String ID[] = {"Aon", "Benz", "Kai", "Khem", "Tan", "Morgan", "C. Korrawit"};
-const String IDCODE[] = {"1909", "0490", "2042", "6969", "0023", "2034", "5555"};
+#define IDLENGTH 9
+const char ID_0[] PROGMEM = "Aon";
+const char IDCODE_0[] PROGMEM = "1909";
+const char ID_1[] PROGMEM = "Benz";
+const char IDCODE_1[] PROGMEM = "0490";
+const char ID_2[] PROGMEM = "Kai";
+const char IDCODE_2[] PROGMEM = "2034";
+const char ID_3[] PROGMEM = "Khem";
+const char IDCODE_3[] PROGMEM = "4724";
+const char ID_4[] PROGMEM = "Tan";
+const char IDCODE_4[] PROGMEM = "0023";
+const char ID_5[] PROGMEM = "Morgan";
+const char IDCODE_5[] PROGMEM = "2042";
+const char ID_6[] PROGMEM = "C. Korrawit";
+const char IDCODE_6[] PROGMEM = "9012";
+const char ID_7[] PROGMEM = "W. Worachai";
+const char IDCODE_7[] PROGMEM = "5555";
+const char ID_8[] PROGMEM = "Nuty";
+const char IDCODE_8[] PROGMEM = "3782";
+const char* const ID[] PROGMEM = {ID_0, ID_1, ID_2, ID_3, ID_4, ID_5, ID_6, ID_7, ID_8};
+const char* const IDCODE[] PROGMEM = {IDCODE_0, IDCODE_1, IDCODE_2, IDCODE_3, IDCODE_4, IDCODE_5, IDCODE_6, IDCODE_7, IDCODE_8};
+char buffer[30];
 String thisID = "";
 String OTP = String(random(1000, 999999));
 String thisPWD = "";
@@ -38,8 +58,10 @@ int index(String key)
     return -1;
   for (int i = 0; i < IDLENGTH; i++)
   {
-    if ( key == IDCODE[i])
+    strcpy_P(buffer, (char*)pgm_read_word(&(IDCODE[i])));
+    if ( key == buffer)
     {
+
       return i;
     }
   }
@@ -48,8 +70,6 @@ int index(String key)
 
 void printLockState(bool lockState)
 {
-  //  lcd.clear();
-  //  delay(50);
   if (lockState)
   {
     lcd.setCursor(3, 1);
@@ -95,83 +115,92 @@ void setup() {
   printLockState(lockState);
   lcd.setCursor(0, 0);
   lcd.print("ID: ");
+  OTP = "12345";
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lcd.setCursor(0, 0);
-  lcd.print("ID: " + thisID);
   key = keypad.getKey();
-  if (key != NO_KEY && key != '#' && key != '*') {
-    thisID += key;
-    delay(50);
-  }
-  if (key == '#')
+  if (!haveID)
   {
-    lcd.clear();
-    thisID = thisID.substring(0, thisID.length() - 1);
-    delay(50);
-  }
-  if (key == '*')
-  {
-    lcd.clear();
+    if (key != NO_KEY && key != '#' && key != '*')
+    {
+      thisID += key;
+    }
+    if (key == '#' && thisID.length() > 0)
+    {
+      thisID = thisID.substring(0, thisID.length() - 1);
+      lcd.clear();
+    }
+    if (key == '*')
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      if (index(thisID) != -1)
+      {
+        lcd.print("ID found");
+        printLockState(lockState);
+        lcd.clear();
+        strcpy_P(buffer, (char*)pgm_read_word(&(ID[index(thisID)])));
+        lcd.setCursor((8 - String(buffer).length()) / 2, 0);
+        lcd.print("Hello! ");
+        lcd.print(String(buffer).substring(0, 7));
+        printLockState(lockState);
+        haveID = true;
+        delay(1000);
+      }
+      else
+      {
+        lcd.print("ID not found");
+        printLockState(lockState);
+        delay(1000);
+      }
+      thisID = "";
+      lcd.clear();
+    }
+    printLockState(lockState);
     lcd.setCursor(0, 0);
-    if (index(thisID) != -1)
-      lcd.print("ID found");
-    else
-      lcd.print("ID not found");
-    delay(2000);
-    lcd.clear();
-    thisID = "";
-    delay(50);
+    lcd.print("ID: " + thisID);
+    if (haveID)
+      lcd.clear();
   }
-  printLockState(lockState);
-  //  char key = keypad.getKey();
-  //  if(haveID == false)
-  //  {
-  //
-  //    printLockState(lockState);
-  //    lcd.setCursor(0,0);
-  //    lcd.print("ID: ");
-  //    lcd.print(thisID);
-  ////        lcd.setCursor(0,0);
-  //  if(key!=NO_KEY)
-  //    lcd.print(key);
-  //    if(key != NO_KEY && key != '#' && key != '*')
-  //    {
-  //      thisID += key;
-  //    }
-  //    if(key != NO_KEY && key == '#')
-  //    {
-  //      lcd.clear();
-  //      printLockState(lockState);
-  //      lcd.setCursor(0,0);
-  //      if(index(thisID) != -1)
-  //      {
-  //        haveID = true;
-  //        lcd.print(index(thisID));
-  ////        delay(1000);
-  //      }
-  //      else
-  //      {
-  //        lcd.print("Failed");
-  //      }
-  //      delay(1000);
-  //      thisID = "";
-  //      lcd.clear();
-  //    }
-  //    if(key != NO_KEY && key == '*')
-  //    {
-  //          lcd.setCursor(0,1);
-  //        lcd.print("*");
-  //      thisPWD = thisPWD.substring(0, thisPWD.length() - 1);
-  //      lcd.clear();
-  //    }
-  //  }
-  //  else
-  //  {
-  //    lcd.clear();
-  //    lcd.setCursor(0,0);
-  //    lcd.print("fuck");
-  //  }
+  else
+  {
+    if (key != NO_KEY && key != '#' && key != '*')
+    {
+      thisPWD += key;
+    }
+    if (key == '#')
+    {
+      thisPWD = thisPWD.substring(0, thisPWD.length() - 1);
+      lcd.clear();
+    }
+    if (key == '*')
+    {
+      if (thisPWD == OTP)
+      {
+        lcd.clear();
+        lockState = not lockState;
+        lcd.print("Success");
+        delay(1000);
+        haveID = false;
+        lcd.clear();
+      }
+      else
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Failed");
+        printLockState(lockState);
+        delay(1000);
+        lcd.clear();
+      }
+      thisPWD = "";
+    }
+    printLockState(lockState);
+    lcd.setCursor(0, 0);
+    lcd.print("PWD: ");
+    lcd.print(thisPWD);
+  }
+  delay(50);
 }
